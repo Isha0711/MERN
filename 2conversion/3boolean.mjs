c337ed3c92fb27d4a3ee2f5bde50049278f245be
Isha Pant <83500671+Isha0711@users.noreply.github.com>
2024-06-25 12:43:32 +0545

    Conversion of Data types
    Converting of Number, String and Boolean data type to one another

---
+++ 2conversion/3boolean.mjs
@@ -0,0 +1,8 @@
+console.log(Boolean("isha")) //true
+console.log(Boolean(""))     //false
+console.log(Boolean("i"))    //true
+console.log(Boolean(" "))    //true
+
+console.log(Boolean(0))      //false
+console.log(Boolean(1))      //true
+console.log(Boolean(102))    //true
